b24a80404c49d22c52cbecce732bb491dd45da7c
Stefan Huber <stefanhuber@Stefans-MacBook-Pro.local>
2015-04-19 21:01:50 +0200

    Routes Password fuer Facebook Callback anpassen und Foto und Name entferen aus User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :passwords => "passwords" }
   resources :offer_blackboards
 
   # The priority is based upon order of creation: first created -> highest priority.
