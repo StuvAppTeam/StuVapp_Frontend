c41465177792cba4aa57642d94359ddfe46db8a6
Stefan Huber <stefanhuber@Stefans-MacBook-Pro.local>
2015-04-20 15:15:43 +0200

    Facebook Login test

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,4 @@ end
     end
   end
 
-  def apply_omniauth(omniauth)
-    self.email = omniauth['user_info']['email'] if email.blank?
-    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
-  end
-
 end
