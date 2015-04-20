c4243a2c353245e572145877c4072549506ca4c0
Stefan Huber <stefanhuber@Stefans-MacBook-Pro.local>
2015-04-20 13:47:53 +0200

    Facebook direkte User erstellung bei Login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      #redirect_to new_user_registration_url
+      user = User.new
+      user.apply_omniauth(auth)
+      if user.save(:validate => false)
+        flash[:notice] = "Account created and signed in successfully."
+        sign_in_and_redirect(:user, user)
+      else
+        flash[:error] = "Error while creating a user account. Please try again."
+        redirect_to root_url
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,9 @@ end
       end
     end
   end
+  def apply_omniauth(omniauth)
+    self.email = omniauth['user_info']['email'] if email.blank?
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end
+
 end
