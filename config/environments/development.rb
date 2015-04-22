a03d355f5b4f1e6d7e195cd12cf9019d1c91d979
Stefan Huber <stefanhuber@Stefans-MacBook-Pro.local>
2015-04-22 16:22:37 +0200

    Berechtigung einbauen, bei User admin Standard auf false setzten

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  #COnfig welche Sprachenen es Laden soll für die Übersetzung
-  config.i18n.available_locales = ['de', :de]
 end
