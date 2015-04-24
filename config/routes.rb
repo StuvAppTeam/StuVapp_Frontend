07d3e47c5905a4a22fc8b0360f6cefc115efc015
ijens <hoffmann.je@stud.dhbw-ravensburg.de>
2015-04-24 12:42:31 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :mensas
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :passwords => "passwords" }
   resources :offer_blackboards
-  resources :mensas
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
