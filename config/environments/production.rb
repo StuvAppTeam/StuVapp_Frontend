52ab580e7f9ecc52ce825a25db4f31423218d4af
Stefan Huber <stefanhuber@Stefans-MacBook-Pro.local>
2015-06-05 13:51:52 +0200

    Anpassung Email auf Gmail Envoirment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,16 +62,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {:host => 'stuvapp.heroku.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address        => 'smtp.gmail.com',
-      :port           => 587,
-      :domain         => 'stuvapp.heroku.com',
+  ActionMailer::Base.smtp_settings = {
+      :tls => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
       :authentication => :login,
-      :user_name      => 'dhbw.mobilesolutions@gmail.com',
-      :password       => 'StuVapp14'
+      :user_name => "dhbw.mobilesolutions@gmail.com",
+      :password => "StuvApp14"
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
