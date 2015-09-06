1fe9c65b9561c41fd0b64142136640f6562e27f1
Tetse <Tetsuya@T-Air.local-muc-47>
2015-09-06 22:46:15 +0200

    FB Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,26 @@ end
     end
   end
 
+def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  if user
+    return user
+  else
+    registered_user = User.where(:email => auth.info.email).first
+
+    return registered_user if registered_user
+
+    user = User.new(
+                   name:auth.extra.raw_info.name,
+                   provider:auth.provider,
+                   uid:auth.uid,
+                   email:auth.info.email,
+                   password:Devise.friendly_token[0,20]
+           )
+    user.skip_confirmation!
+    user.save
+    user
+  end
+end
+
 end
